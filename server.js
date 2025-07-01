341d33d2161d5aefe5babbd084e53df435f1dbf4
ramji <ramjib2311@gmail.com>
2025-07-01 17:02:54 +0530

    removed-index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 
 dotenv.config();
 
